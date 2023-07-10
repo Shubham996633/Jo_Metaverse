a0d1fe0ac4c9b733e02ac769985a076ad04b0ff2
Shubham Maurya <65014926+Shubham996633@users.noreply.github.com>
2023-07-11 00:50:11 +0530

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,11 @@ module.exports = {
     browser: true,
     es2021: true,
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   extends: [
     'plugin:react/recommended',
     'airbnb',
